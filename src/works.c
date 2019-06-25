35712a557d23390948356ae50cb4a349657ced0a
Osaki Shuhei <osaki.s.ab@m.titech.ac.jp>
2019-06-25 13:55:08 +0900

    henkou

--- src/works.c
+++ src/works.c
@@ -6,12 +6,36 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i, j lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+    lb = -1;
+    ub = n;
+    int m = 1;
+    int a = 0;
+    while(ub - lb > 1){
+        int x = (lb + ub)/2;
+        for(j = 0; j < n; j++){
+            if(x<A[j]){
+                m = m + k;
+                break;
+            }
+            if(a+A[j]>x){
+                a=A[j];
+                m=m+1;
+            }
+            else{
+                a=a+A[j];
+            }
+            }
+            if(m > k) ib = x;
+        else ub = x;
+    
+    
+    printf("%d\n", ub);
 
   return 0;
 }
