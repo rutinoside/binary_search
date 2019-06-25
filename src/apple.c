7f1ffe2754a05641fa9c7232acfd0e00eaa4cb0a
Osaki Shuhei <osaki.s.ab@m.titech.ac.jp>
2019-06-25 12:55:28 +0900

    apple 変更

--- src/apple.c
+++ src/apple.c
@@ -6,12 +6,24 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i, j, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    
+    lb = -1;
+    ub = n;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        for(i = 0; i < n; i++){
+           a=a+(A[j]+m-1)/m
+        }
+        if(a>= k) lb = m;
+        else ub = m;
+    }
+    
+    printf("%d\n", ub);
 
   return 0;
 }
